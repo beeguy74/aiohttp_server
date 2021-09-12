0aae01ce9ea12cce7f3ca40fdb04d509cf580ae3
beeguy <beeguy74@gmail.com>
2021-09-12 16:52:19 +0300

    some changes

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,6 +1,5 @@
 import typing
 
-from app.admin.views import AdminCurrentView
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
@@ -8,6 +7,7 @@ if typing.TYPE_CHECKING:
 
 def setup_routes(app: "Application"):
     from app.admin.views import AdminLoginView
+    from app.admin.views import AdminCurrentView
 
     app.router.add_view("/admin.login", AdminLoginView)
     app.router.add_view("/admin.current", AdminCurrentView)
--- app/web/routes.py
+++ app/web/routes.py
@@ -1,5 +1,7 @@
 from aiohttp.web_app import Application
 from app.crm.routes import setup_routes as crm_setup_routes
+from app.admin.routes import setup_routes as admin_setup_routes
 
 def setup_routes(app: Application):
     crm_setup_routes(app)
+    admin_setup_routes(app)
